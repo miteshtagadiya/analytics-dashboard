b30e647b999dcb0df1b38f32eeee2a52a170763f
mitesh <miteshtagadiya@gmail.com>
2020-05-07 21:41:33 +0530

    Updates

---
+++ precache-manifest.1e105ddbc63ea9d4a74e7d95a8b539ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "545fad02f94744ce9e18695c93816579",
+    "url": "/analytics-dashboard/index.html"
+  },
+  {
+    "revision": "65de2a094cd843b42eed",
+    "url": "/analytics-dashboard/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "d2f56e363f9143c01754",
+    "url": "/analytics-dashboard/static/css/main.92462a38.chunk.css"
+  },
+  {
+    "revision": "65de2a094cd843b42eed",
+    "url": "/analytics-dashboard/static/js/2.148d1e24.chunk.js"
+  },
+  {
+    "revision": "ca3cfcda77dc9f4f71fda9fdfcf14fb0",
+    "url": "/analytics-dashboard/static/js/2.148d1e24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2f56e363f9143c01754",
+    "url": "/analytics-dashboard/static/js/main.ad496964.chunk.js"
+  },
+  {
+    "revision": "071976a7155de5693d6a",
+    "url": "/analytics-dashboard/static/js/runtime-main.7390b0ed.js"
+  }
+]);
